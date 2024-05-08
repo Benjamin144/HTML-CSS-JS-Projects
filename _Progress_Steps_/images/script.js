8d970ef30bb0dacdce683c103ce783ffbc88ff50
Benjamin144 <josephroberts@email.com>
2024-05-08 17:14:20 +0000

    added circle progress functionality forEach circle in pprogress bar when active

--- _Progress_Steps_/images/script.js
+++ _Progress_Steps_/images/script.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById('progress')
 const next = document.getElementById('next')
 const prev = document.getElementById('prev')
-const circles = document.querySelectorAll('.circle')
+const circles = document.querySelectorAll('.circle') // because we are working with more than one circle we can use the querySelectorAll to enact a node list
 
 let currentActive = 1   
 
@@ -12,8 +12,30 @@ next.addEventListener('click', () => {
         currentActive = circles.length
     }
 
-console.log(currentActive)
+    update()
+})
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
 })
 
-//.addEventListener('click', () =>
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+    
+}
+
+
 
